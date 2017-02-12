4e7e2a557bf550cc0642b3fa49804e67f712bcdd
Zhao vistart <i@vistart.vip>
2017-02-12 18:07:06 +0800

    add specified config for console.

--- console/config/main.php
+++ console/config/main.php
@@ -7,7 +7,7 @@ $params = array_merge(
 );
 
 return [
-    'id' => 'app-console',
+    'id' => 'rhosocial-console',
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
     'controllerNamespace' => 'console\controllers',
